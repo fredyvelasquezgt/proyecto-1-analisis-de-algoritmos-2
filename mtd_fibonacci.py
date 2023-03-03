20e47a69c3ea9fec16fbf3890853ea0011a7f0a9
Fredy Velásquez <59968764+fredyvelasquezgt@users.noreply.github.com>
2023-03-03 17:33:40 -0600

    Add files via upload

---
+++ mtd_fibonacci.py
@@ -0,0 +1,78 @@
+import time
+
+def fibonacci(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fibonacci(n-1) + fibonacci(n-2)
+
+def mtd_fibonacci(n):
+    start_time = time.time()
+    
+    state = 'q0'
+    tape = ['B'] * (n+2)
+    tape[1] = '1'
+    head_pos = 1
+    count = 1
+    
+    while state != 'qf':
+        print(state + ': ' + ' '.join(tape[:head_pos]) + ' [' + tape[head_pos] + '] ' + ' '.join(tape[head_pos+1:]))
+        
+        if state == 'q0':
+            state = 'q1'
+        
+        elif state == 'q1':
+            if tape[head_pos] == 'B':
+                state = 'qf'
+            else:
+                tape[head_pos+1] = '1'
+                head_pos += 1
+                count += 1
+                if count == n:
+                    state = 'qf'
+                else:
+                    state = 'q2'
+        
+        elif state == 'q2':
+            if tape[head_pos] == 'B':
+                state = 'qf'
+            else:
+                tape[head_pos-1] = '1'
+                head_pos -= 1
+                count += 1
+                if count == n:
+                    state = 'qf'
+                else:
+                    state = 'q3'
+        
+        elif state == 'q3':
+            if tape[head_pos] == 'B':
+                state = 'qf'
+            else:
+                tape[head_pos] = 'B'
+                head_pos += 1
+                count += 1
+                if count == n:
+                    state = 'qf'
+                else:
+                    state = 'q4'
+        
+        elif state == 'q4':
+            if tape[head_pos] == 'B':
+                state = 'qf'
+            else:
+                tape[head_pos-1] = 'B'
+                head_pos -= 1
+                count += 1
+                if count == n:
+                    state = 'qf'
+                else:
+                    state = 'q1'
+    
+    end_time = time.time()
+    print('TIempo:', end_time - start_time)
+
+n = int(input('Ingrese un número para calcular el número correspondiente en la secuencia de Fibonacci: '))
+mtd_fibonacci(n)
